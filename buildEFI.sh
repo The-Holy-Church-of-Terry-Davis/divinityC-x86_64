eccbe77cdbe288a50a49f3264e4ac8be0056c471
YendisFish <brintonsid@gmail.com>
2023-08-22 13:15:28 -0600

    bug fixes in build script

--- buildEFI.sh
+++ buildEFI.sh
@@ -1,13 +1,23 @@
+#!/usr/bin/bash
+
 if [ "$EUID" -ne 0 ]; then
   echo "This script must be run with superuser privileges. Exiting."
   exit 1
 fi
 
+echo "Attempting to update/install required packages"
+sudo apt-get install -y qemu-utils qemu-system-x86 dotnet-sdk-7.0
+
+chmod +x ./EFI/compileEFI.sh
+chmod +x ./EFI/mountDrive.sh
+
 echo "Compiling EFI"
-./EFI/compileEFI.sh
+sudo ./EFI/compileEFI.sh
 echo "Compiled"
+
 echo "Mounting drive.hdd"
-./EFI/mountDrive.sh
+sudo ./EFI/mountDrive.sh
 echo "Mounted"
+
 echo "Moving files to drive.hdd"
 sudo mv ./EFI/BOOTX64.EFI /mnt/G/EFI/Boot
